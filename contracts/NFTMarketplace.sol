98805de78a74e15e4f296e0efa1837b0cc7cd4f2
sanjay-in <sanjaysridharan@outlook.com>
2024-10-26 21:11:29 +0530

    create contract

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    address payable private immutable i_owner;
+
+    constructor() ERC721("NFTMarketplace", "NFT") {
+        i_owner = payable(msg.sender);
+    }
+}
