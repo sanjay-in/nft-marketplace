024170a8429c83290b42bf8c8e78fad6bc4285b1
sanjay-in <sanjaysridharan@outlook.com>
2024-10-26 22:36:34 +0530

    update solidity version

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
