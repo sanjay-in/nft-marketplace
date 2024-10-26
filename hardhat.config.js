2c5f68e171a4cd98b42c64dbd7c86a9ba3aca4d3
sanjay-in <sanjaysridharan@outlook.com>
2024-10-26 22:36:57 +0530

    add hardhat config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,39 @@
+require("hardhat-deploy");
+require("solidity-coverage");
+require("dotenv").config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 module.exports = {
   solidity: "0.8.27",
+  defaultNetwork: "hardhat",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    sepolia: {
+      chainId: 11155111,
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
 };
