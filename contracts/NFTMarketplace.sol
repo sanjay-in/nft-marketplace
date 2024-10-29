28c8767a921ece714978fa157465ae23b5bbdf39
sanjay-in <sanjaysridharan@outlook.com>
2024-10-29 20:09:14 +0530

    add modifiers and errors

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+// Errors
+error NFTMarketplace__NotOwner();
+error NFTMarketplace__PriceCannotBeZero();
+error NFTMarketplace__ListingPriceNotEqual();
+
 contract NFTMarketplace is ERC721URIStorage {
     // Type declaration
     struct ListedToken {
@@ -28,6 +33,28 @@ contract NFTMarketplace is ERC721URIStorage {
         uint256 price,
         bool sold
     );
+
+    // Modifiers
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) {
+            revert NFTMarketplace__NotOwner();
+        }
+        _;
+    }
+
+    modifier priceNotZero(uint256 _price) {
+        if (_price <= 0) {
+            revert NFTMarketplace__PriceCannotBeZero();
+        }
+        _;
+    }
+
+    modifier listingPriceMatch(uint256 _price) {
+        if (msg.value != i_listingPrice) {
+            revert NFTMarketplace__ListingPriceNotEqual();
+        }
+        _;
+    }
 
     constructor(uint256 listingPrice) ERC721("NFTMarketplace", "NFT") {
         i_owner = payable(msg.sender);
