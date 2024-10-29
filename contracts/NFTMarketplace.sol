a2ac8cb613c48bfd0203b1a5ed04b83087b7b7b3
sanjay-in <sanjaysridharan@outlook.com>
2024-10-29 19:54:40 +0530

    add variables and events

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -4,7 +4,29 @@ pragma solidity ^0.8.27;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NFTMarketplace is ERC721URIStorage {
+    // Type declaration
+    struct ListedToken {
+        uint256 tokenId;
+        address payable owner;
+        address payable seller;
+        uint256 price;
+        bool sold;
+    }
+
+    // State variables
     address payable private immutable i_owner;
+    uint256 private s_tokenId;
+    uint256 private s_itemSold;
+    mapping(uint256 => ListedToken) private s_tokenIdToListedToken;
+
+    //Events
+    event TokenListed(
+        uint256 tokenId,
+        address payable owner,
+        address payable seller,
+        uint256 price,
+        bool sold
+    );
 
     constructor() ERC721("NFTMarketplace", "NFT") {
         i_owner = payable(msg.sender);
