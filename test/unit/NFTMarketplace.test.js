a8336ec889e55ad3a7c9fe12026aca9bb3670bc5
sanjay-in <sanjaysridharan@outlook.com>
2024-10-30 23:30:34 +0530

    update constructor test

--- test/unit/NFTMarketplace.test.js
+++ test/unit/NFTMarketplace.test.js
@@ -7,6 +7,9 @@ const { developmentChains } = require("../../helper-hardhat.config.js");
       let expect;
       let contract, nft;
       let deployer, addresses;
+      const tokenURI =
+        "https://v2.emblemvault.io/v3/meta/0x7E6027a6A84fC1F6Db6782c523EFe62c923e46ff/42741568268271627005603014856691612673583670625740997034891561691910895568241";
+      const price = 0.01;
       beforeEach(async () => {
         const chai = await import("chai");
         expect = chai.expect;
@@ -35,6 +38,18 @@ const { developmentChains } = require("../../helper-hardhat.config.js");
         it("checks if deployer is the owner", async () => {
           const owner = await nft.getOwner();
           expect(owner).to.equal(deployer);
+        });
+
+        it("checks if listing price matches", async () => {
+          const listingPrice = ethers.parseEther("0.001");
+          const fetchedListingPrice = await nft.getListingPrice();
+          expect(listingPrice).to.equal(fetchedListingPrice);
+        });
+      });
+
+      describe("createToken", () => {
+        it("reverts if price is zero", async () => {
+          await expect(nft.createToken(tokenURI, 0)).to.be.revertedWithCustomError(nft, "NFTMarketplace__PriceCannotBeZero");
         });
       });
     });
