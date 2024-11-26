dcd0e0a8900468e5fbdc5469c4ddfe51e6792374
sanjay-in <sanjaysridharan@outlook.com>
2024-11-26 22:36:25 +0530

    add react toastify style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home/Home";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
