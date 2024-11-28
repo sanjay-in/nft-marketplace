4e3f58d2ae221c5e2e95b5f947015ea0c4337387
sanjay-in <sanjaysridharan@outlook.com>
2024-11-28 21:58:36 +0530

    add home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import Listing from "../Listing/Listing";
+import NFTPage from "../NFTPage/NFTPage";
+import MyNFT from "../MyNFT/MyNFT";
+import Error from "../Error/Error";
+import "./Home.css";
+
+const Home = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Listing />,
+      errorElement: <Error />,
+    },
+    {
+      path: "/myNFTs",
+      element: <MyNFT />,
+    },
+    {
+      path: `/nft`,
+      element: <NFTPage />,
+    },
+  ]);
+  return (
+    <>
+      <Navbar />
+      <RouterProvider router={router} />
+    </>
+  );
+};
+
+export default Home;
